3a5f3eb63b3d8afb29f81d3bcf6c83add84a9c1c
Malachi Pineda <mchipin@hotmail.com>
2023-03-20 21:42:46 -0600

    added oop js homework

---
+++ homeWork/oop-in-javascript/main.js
@@ -0,0 +1,71 @@
+
+// Object literal
+
+    // const circle = {
+    //     radius: 1,
+    //     location: {
+    //         x: 1,
+    //         y: 1
+    //     },
+    //     draw: function() {
+    //         console.log('draw');
+    //     }
+    // };
+
+    // circle.draw();
+
+// Factory Function
+
+    // function createCircle(radius, location){
+    //     return {
+    //         radius,
+    //         draw: function() {
+    //             console.log('draw');
+    //         }
+    //     };
+    // }
+
+    // const circle = createCircle(1);
+
+// Constructor Function
+
+    // function Circle(radius) {
+    //     this.radius = radius;
+    //     this.draw = function() {
+    //     console.log('draw');
+    //     }
+    // }
+
+    // const another = new Circle(1);
+
+// Primitives
+
+    // let x = 10;
+    // let y = x;
+
+    // x = 20;
+
+    // let number = 10
+
+    // function increase(number) {
+    //     number++;
+    // }
+
+    // increase(number);
+    // console.log(number);
+
+// Objects
+
+    // let x = { value: 10 };
+    // let y = x;
+
+    // x.value = 20;
+
+    // let obj = { value: 10 };
+
+    // function increase(obj) {
+    //     obj.value++;
+    // }
+
+    // increase(obj);
+    // console.log(obj);
