781c685d79effb41513361392999a9283f4bd6d7
Malachi Pineda <mchipin@hotmail.com>
2023-04-19 19:35:43 -0600

    completed JS way chapter 9

--- homeWork/class-16-js-way/thejsway-starter-code-main/chapter09/js/character_inventory.js
+++ homeWork/class-16-js-way/thejsway-starter-code-main/chapter09/js/character_inventory.js
@@ -4,6 +4,48 @@ Character inventory
 
 // TODO: define the Character class here
 
+class Character {
+    constructor(name, health, strength) {
+      this.name = name;
+      this.health = health;
+      this.strength = strength;
+      this.xp = 0; // XP is always zero for new characters
+      this.gold = 10 // Each character begins with 10 gold
+      this.keys = 1 // Each character begins with 1 key
+    }
+    // Attack a target
+    attack(target) {
+      if (this.health > 0) {
+        const damage = this.strength;
+        console.log(
+          `${this.name} attacks ${target.name} and causes ${damage} damage points`
+        );
+        target.health -= damage;
+        if (target.health > 0) {
+          console.log(`${target.name} has ${target.health} health points left`);
+        } else {
+          target.health = 0;
+          const bonusXP = 10;
+          console.log(
+            `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points, ${target.gold} gold and ${target.keys} key(s)`
+          );
+          this.xp += bonusXP;
+          this.gold += target.gold;
+          this.keys += target.keys
+          target.gold -= target.gold;
+          target.keys -= target.keys;
+        }
+      } else {
+        console.log(`${this.name} can’t attack (they've been eliminated)`);
+      }
+    }
+    // Return the character description
+    describe() {
+      return `${this.name} has ${this.health} health points, ${this
+        .strength} as strength and ${this.xp} XP points, ${this.gold} gold and ${this.keys} key(s)`;
+    }
+}
+
 const aurora = new Character("Aurora", 150, 25);
 const glacius = new Character("Glacius", 130, 30);
 
