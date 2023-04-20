48c12783675914555f5f22f69df6f5c9142ab769
Malachi Pineda <mchipin@hotmail.com>
2023-04-19 19:35:43 -0600

    completed JS way chapter 9

--- homeWork/class-16-js-way/thejsway-starter-code-main/chapter09/js/account_list.js
+++ homeWork/class-16-js-way/thejsway-starter-code-main/chapter09/js/account_list.js
@@ -1,3 +1,27 @@
 /*
 Bank account list
 */
+
+class BankAccount {
+    constructor(name) {
+        this.name = name;
+        this.balance = 0;
+    }
+    credit(amount) {
+        this.balance += amount;
+    }
+    describe() {
+        return `Owner: ${this.name}, balance: ${this.balance}`;
+    }
+}
+
+const accountList = [
+    new BankAccount('Sean'),
+    new BankAccount('Brad'),
+    new BankAccount('Georges')
+]
+
+for (let account of accountList) {
+    account.credit(1000)
+    console.log(account.describe())
+}
--- homeWork/class-16-js-way/thejsway-starter-code-main/chapter09/js/dogs.js
+++ homeWork/class-16-js-way/thejsway-starter-code-main/chapter09/js/dogs.js
@@ -4,6 +4,21 @@ Dog objects
 
 // TODO: define the Dog class here
 
+class Dog {
+    constructor(name, species, size){
+        this.name = name
+        this.species = species
+        this.size = size
+    }
+    bark(){
+        if (this.size > 60) {
+            return 'Grrr! Grrr!'
+        } else  {
+            return 'Woof! Woof!'
+        }
+    }
+}
+
 const fang = new Dog("Fang", "boarhound", 75);
 console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
 console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
