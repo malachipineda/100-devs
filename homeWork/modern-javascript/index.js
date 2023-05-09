4afb1b9d99b18b9968704342d01fbb062c07ab5d
Malachi Pineda <mchipin@hotmail.com>
2023-05-08 20:29:29 -0600

    modern-javascript practice initial commit

---
+++ homeWork/modern-javascript/index.js
@@ -0,0 +1,6 @@
+var moment = require('moment');
+
+console.log("Hello from JavaScript!!!")
+console.log(moment().startOf('day').fromNow());
+console.log(moment().endOf('day').fromNow());
+console.log(x);
