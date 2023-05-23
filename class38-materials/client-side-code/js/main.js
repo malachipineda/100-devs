404f49f1f1653c3bb31e1712b650f9410b58becd
Malachi Pineda <mchipin@hotmail.com>
2023-05-22 23:35:46 -0600

    moved crud demo and fullstack open project to their own repos

---
+++ class38-materials/client-side-code/js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://tame-plum-anemone-tam.cyclic.app/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
